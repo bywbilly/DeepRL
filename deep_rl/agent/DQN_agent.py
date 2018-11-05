fix bugs for statistial gradients

deep_rl/agent/DQN_agent.py
@@ -92,7 +92,7 @@ class DQNAgent(BaseAgent):
             experiences.append([state, action, reward, next_state, done])
         self.replay.feed_batch(experiences)
 
-        if self.total_steps > self.config.exploration_steps:
+        if (self.total_steps > self.config.exploration_steps):
             experiences = self.replay.sample()
             states, actions, rewards, next_states, terminals = experiences
             states = self.config.state_normalizer(states)
@@ -111,20 +111,46 @@ class DQNAgent(BaseAgent):
             q_next = self.config.discount * q_next * (1 - terminals)
             q_next.add_(rewards)
             actions = tensor(actions).long()
-            q = self.network(states, half=self.config.half)
+            save_gradient = False
+            if (self.config.gradient_step and (self.total_steps % self.config.gradient_step == 0)):
+                save_gradient = True
+            q = self.network(states, half=self.config.half, save_gradient=save_gradient)
             q = q[self.batch_indices, actions]
             loss = (q_next - q).pow(2).mul(0.5).mean()
             self.writer.add_scalar("Train_mean_loss", loss, self.total_steps)
             self.optimizer.zero_grad()
             loss.backward()
-            if self.config.gradient_step and (self.total_steps % self.config.gradient_step == 0):
+            if (self.config.gradient_step and (self.total_steps % self.config.gradient_step == 0)):
                 for name, para in self.network.named_parameters():
-                    self.writer.add_histogram(name + "before_clip", para.grad.clone().cpu().data.numpy(), self.total_steps)
+                    log2_grad = []
+                    for item in para.grad.clone().cpu().data.numpy().flatten(-1):
+                        if item == 0:
+                            log2_grad.append(0)
+                        else:
+                            log2_grad.append(int(np.log(abs(item))))
+                    self.writer.add_histogram(name + "before_clip", np.array(log2_grad), self.total_steps)
             nn.utils.clip_grad_norm_(self.network.parameters(), self.config.gradient_clip)
             #tmp_grads = []
-            if self.config.gradient_step and (self.total_steps % self.config.gradient_step == 0):
+            if (self.config.gradient_step and (self.total_steps % self.config.gradient_step == 0)):
                 for name, para in self.network.named_parameters():
-                    self.writer.add_histogram(name + "after_clip", para.grad.clone().cpu().data.numpy(), self.total_steps)
+                    log2_grad = []
+                    for item in para.grad.clone().cpu().data.numpy().flatten(-1):
+                        if item == 0:
+                            log2_grad.append(0)
+                            self.config.gradient_summary.append(0)
+                        else:
+                            log2_grad.append(int(np.log(abs(item))))
+                            self.config.gradient_summary.append(int(np.log(abs(item))))
+                    self.writer.add_histogram(name + "after_clip", np.array(log2_grad), self.total_steps)
+            if self.total_steps == 1:
+                self.writer.add_histogram("normal gradient 1", self.config.gradient_summary, self.total_steps)
+                self.writer.add_histogram("activation gradient 1", self.config.activation_gradient_summary, self.total_steps)
+            if self.total_steps == int(1e7):
+                self.writer.add_histogram("normal gradient 1e7", self.config.gradient_summary, self.total_steps)
+                self.writer.add_histogram("activation gradient 1e7", self.config.activation_gradient_summary, self.total_steps)
+            if self.total_steps == int(1e7 + 5e6):
+                self.writer.add_histogram("normal gradient 1.5e7", self.config.gradient_summary, self.total_steps)
+                self.writer.add_histogram("activation gradient 1.5e7", self.config.activation_gradient_summary, self.total_steps)
             with config.lock:
                 self.optimizer.step()
 

deep_rl/network/network_bodies.py
@@ -5,22 +5,42 @@
 #######################################################################
 
 from .network_utils import *
+import numpy as np
+
+def save_grad(summary_writer):
+    def hook(grad):
+        for item in grad.cpu().data.numpy().flatten(-1):
+            if item == 0:
+                summary_writer.append(0)
+            else:
+                summary_writer.append(int(np.log(abs(item))))
+    return hook
 
 class NatureConvBody(nn.Module):
-    def __init__(self, in_channels=4):
+    def __init__(self, in_channels=4, activation_gradient_summary=None):
         super(NatureConvBody, self).__init__()
+        self.activation_gradient_summary = activation_gradient_summary
         self.feature_dim = 512
         self.conv1 = layer_init(nn.Conv2d(in_channels, 32, kernel_size=8, stride=4))
         self.conv2 = layer_init(nn.Conv2d(32, 64, kernel_size=4, stride=2))
         self.conv3 = layer_init(nn.Conv2d(64, 64, kernel_size=3, stride=1))
         self.fc4 = layer_init(nn.Linear(7 * 7 * 64, self.feature_dim))
 
-    def forward(self, x):
+    def forward(self, x, save_gradient=False):
+        # Register hook function to store the value of the intermediate gradient
         y = F.relu(self.conv1(x))
+        if save_gradient:
+            y.register_hook(save_grad(self.activation_gradient_summary))
         y = F.relu(self.conv2(y))
+        if save_gradient:
+            y.register_hook(save_grad(self.activation_gradient_summary))
         y = F.relu(self.conv3(y))
+        if save_gradient:
+            y.register_hook(save_grad(self.activation_gradient_summary))
         y = y.view(y.size(0), -1)
         y = F.relu(self.fc4(y))
+        if save_gradient:
+            y.register_hook(save_grad(self.activation_gradient_summary))
         return y
 
 class DDPGConvBody(nn.Module):

deep_rl/network/network_heads.py
@@ -8,17 +8,18 @@ from .network_utils import *
 from .network_bodies import *
 
 class VanillaNet(nn.Module, BaseNet):
-    def __init__(self, output_dim, body):
+    # Activation gradient summary is used to store the intermediate gradient 
+    def __init__(self, output_dim, body, activation_gradient_summary=None):
         super(VanillaNet, self).__init__()
         self.fc_head = layer_init(nn.Linear(body.feature_dim, output_dim))
         self.body = body
         self.to(Config.DEVICE)
 
-    def forward(self, x, half=False):
+    def forward(self, x, half=False, save_gradient=False):
         if half:
-            phi = self.body(tensor(x).half())
+            phi = self.body(tensor(x).half(), save_gradient)
         else:
-            phi = self.body(tensor(x))
+            phi = self.body(tensor(x), save_gradient)
         y = self.fc_head(phi)
         return y
 

examples.py
@@ -5,6 +5,7 @@
 #######################################################################
 
 from deep_rl import *
+import pickle
 
 # DQN
 def dqn_cart_pole(): 
@@ -36,6 +37,8 @@ def dqn_cart_pole():
 
 def dqn_pixel_atari(name):
     config = Config()
+    config.tensorboard = "/home/fs01/yb263/dqn_atari_tfboard_test"
+    mkdir(config.tensorboard)
     config.history_length = 4
     config.task_fn = lambda: PixelAtari(name, frame_skip=4, history_length=config.history_length,
                                         log_dir=get_default_log_dir(dqn_pixel_atari.__name__))
@@ -44,7 +47,9 @@ def dqn_pixel_atari(name):
 
     config.optimizer_fn = lambda params: torch.optim.RMSprop(
         params, lr=0.00025, alpha=0.95, eps=0.01, centered=True)
-    config.network_fn = lambda: VanillaNet(config.action_dim, NatureConvBody(in_channels=config.history_length))
+    gradient_summary = []
+    activation_gradient_summary = []
+    config.network_fn = lambda: VanillaNet(config.action_dim, NatureConvBody(in_channels=config.history_length, activation_gradient_summary=activation_gradient_summary))
     # config.network_fn = lambda: DuelingNet(config.action_dim, NatureConvBody(in_channels=config.history_length))
     config.random_action_prob = LinearSchedule(1.0, 0.01, 1e6)
 
@@ -61,14 +66,25 @@ def dqn_pixel_atari(name):
     config.exploration_steps = 50000
     config.sgd_update_frequency = 4
     config.gradient_clip = 5
+    # For storing activation gradient summary
+    config.gradient_summary = gradient_summary
+    config.activation_gradient_summary = activation_gradient_summary
     # config.double_q = True
     config.double_q = False
     config.max_steps = int(2e7)
+    #config.max_steps = 2
     config.logger = get_logger(file_name=dqn_pixel_atari.__name__)
     run_steps(DQNAgent(config))
 
+    with open("./pickle_results/dqn_atari_activation.pickle", "wb") as f:
+        pickle.dump(activation_gradient_summary, f)
+    with open("./pickle_results/dqn_atari.pickle", "wb") as f:
+        pickle.dump(gradient_summary, f)
+
 def half_dqn_pixel_atari(name):
     config = Config()
+    config.tensorboard = "/home/fs01/yb263/dqn_half_atari_tfboard_test"
+    mkdir(config.tensorboard)
     config.half = True
     config.history_length = 4
     config.task_fn = lambda: PixelAtari(name, frame_skip=4, history_length=config.history_length,
@@ -79,7 +95,9 @@ def half_dqn_pixel_atari(name):
 
     config.optimizer_fn = lambda params: torch.optim.RMSprop(
         params, lr=0.00025, alpha=0.95, eps=0.01, centered=True)
-    config.network_fn = lambda: VanillaNet(config.action_dim, NatureConvBody(in_channels=config.history_length))
+    gradient_summary = []
+    activation_gradient_summary = []
+    config.network_fn = lambda: VanillaNet(config.action_dim, NatureConvBody(in_channels=config.history_length, activation_gradient_summary=activation_gradient_summary))
     # config.network_fn = lambda: DuelingNet(config.action_dim, NatureConvBody(in_channels=config.history_length))
     config.random_action_prob = LinearSchedule(1.0, 0.01, 1e6)
 
@@ -94,13 +112,20 @@ def half_dqn_pixel_atari(name):
     config.exploration_steps = 50000
     config.sgd_update_frequency = 4
     config.gradient_clip = 5
+    # For storing activation gradient summary
+    config.activation_gradient_summary = activation_gradient_summary
+    config.gradient_summary = gradient_summary
     # config.double_q = True
     config.double_q = False
     config.max_steps = int(2e7)
+    #config.max_steps = 2
     config.logger = get_logger(file_name=dqn_pixel_atari.__name__)
     run_steps(DQNAgent(config))
-
-
+    
+    with open("./pickle_results/half_dqn_atari_activation.pickle", "wb") as f:
+        pickle.dump(activation_gradient_summary, f)
+    with open("./pickle_results/half_dqn_atari.pickle", "wb") as f:
+        pickle.dump(gradient_summary, f)
 
 def dqn_ram_atari(name):
     config = Config()